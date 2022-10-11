0fd5a7ca49af6c35b1cf4299843403ad78a245d0
Anders <alex@kipsoft.fr>
2022-10-11 16:31:52 +0200

    feat: new version

--- src/scripts/send_to_pipeline.sh
+++ src/scripts/send_to_pipeline.sh
@@ -4,17 +4,29 @@ PostToCircleCI()
 {
     echo -e "PostToCircleCI \n"
     echo -e $CCI_STATUS"\n"
-    git log --format=format:%s -n 1
+    previous_commit_message=`git log --format=format:%s -n 1 HEAD~1`
+
+    if [[ $CCI_STATUS != "pass" ]]
+    then
+        echo -e "Job failed, we skip."
+        exit 0
+    fi
 
 
-    #curl --location \
-    #          --request POST '' \
-    #          --header 'Content-Type: application/json' \
-    #          -d '' \
-    #          -u ":"
-    echo
+    # If the previous commit right before the update_version was from the update migration pipeline we skip it
+    if [[ $previous_commit_message == *"SKIP_MIGRATION_STEP"* ]]
+    then
+        echo -e "We will skip the update_migration pipeline as we already come from it."
+    else
+        echo -e "Now we must trigger the update_migration pipeline."
+
+        echo -e will exec: curl --location --request POST \'${PIPELINE_URL}\' --header \'Content-Type: application/json\' -d \'{\"parameters\": {\"gem\": \"${PIPELINE_PARAMS}\"}}\' -u \'${PIPELINE_PERSONAL_TOKEN}:\'
+        curl --location --request POST ${PIPELINE_URL} --header 'Content-Type: application/json' -d '{"parameters": {"gem": "'${PIPELINE_PARAMS}'"}}' -u "${PIPELINE_PERSONAL_TOKEN}:"
+    fi
+
+    exit 0
 }
 
-. "/tmp/BLURP_JOB_STATUS"
+. "/tmp/BLURP_JOB_VARS"
 
 PostToCircleCI
