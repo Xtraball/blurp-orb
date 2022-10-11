993f61d399fc75095fec053d133c58cd672cab92
Anders <alex@kipsoft.fr>
2022-10-11 14:27:01 +0200

    -m

--- src/scripts/send_to_pipeline.sh
+++ src/scripts/send_to_pipeline.sh
@@ -3,6 +3,18 @@
 PostToCircleCI()
 {
     echo -e "PostToCircleCI \n"
+    echo -e $CCI_STATUS"\n"
+    git log --format=format:%s -n 1
+
+
+    #curl --location \
+    #          --request POST '' \
+    #          --header 'Content-Type: application/json' \
+    #          -d '' \
+    #          -u ":"
+    echo
 }
 
 . "/tmp/BLURP_JOB_STATUS"
+
+PostToCircleCI
